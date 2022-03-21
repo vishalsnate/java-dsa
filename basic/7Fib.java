1b85b9b852c8971616fa9b976872032544475403
Vishal Snate <62849937+vishalsnate@users.noreply.github.com>
2022-03-22 00:10:47 +0530

    Update 7Fib.java

--- basic/7Fib.java
+++ basic/7Fib.java
@@ -18,3 +18,29 @@ class Fib
         System.out.println(b);
     }
 }
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+import java.util.*;
+ 
+class Main
+{
+ public static void main(String args[])
+ {
+         int i,c=0,n;
+ Scanner sc = new Scanner(System.in);
+ System.out.println("Enter a number to generate fibonacci series upto nth term");
+     n=sc.nextInt();
+   int a=0;
+   int b=1;
+ 
+ System.out.println("Fibonacci series upto "+n+" is :-");
+   while(c!=n)
+   {
+       System.out.print(c+" ");
+       a=b;
+       b=c;
+       c=a+b;
+   }
+ }
+}
